c667375e4bc8c965dd17a59587b4b4d1084edeed
Web Dev Simplified <kyle@webdevsimplified.com>
2022-03-14 07:57:01 -0500

    Fix Electron Bug

--- extension.js
+++ extension.js
@@ -1,22 +1,27 @@
 const vscode = require("vscode")
 const axios = require("axios")
 const xmlParser = require("fast-xml-parser")
+const https = require("https")
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
+  https.globalAgent.options.rejectUnauthorized = false
   const res = await axios.get("https://blog.webdevsimplified.com/rss.xml")
-  const articles = xmlParser.parse(res.data).rss.channel.item.map(article => {
-    return {
-      label: article.title,
-      detail: article.description,
-      link: article.link,
-    }
-  })
+  const articles = xmlParser
+    .parse(res.data)
+    .rss.channel.item.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate))
+    .map(article => {
+      return {
+        label: article.title,
+        detail: article.description,
+        link: article.link,
+      }
+    })
 
   let disposable = vscode.commands.registerCommand(
-    "wds-search-blog-example.searchWdsBlogExample",
+    "wds-blog-search.searchWdsBlog",
     async function () {
       const article = await vscode.window.showQuickPick(articles, {
         matchOnDetail: true,
